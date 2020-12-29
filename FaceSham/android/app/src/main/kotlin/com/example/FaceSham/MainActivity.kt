e8626286a9c5dbe6f8e70f266baf5f47cda52a7f
Vignesh Charan Raman <vigneshcharan26@gmail.com>
2020-12-29 13:22:33 +0530

    reorganized

--- FaceSham/android/app/src/main/kotlin/com/example/FaceSham/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.FaceSham
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- hack_nagpur/android/app/src/main/kotlin/com/example/hack_nagpur/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hack_nagpur
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
